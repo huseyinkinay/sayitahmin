8cf656217c745a39d9c81a935b34d7aeb6529682
huseyinkinay <huseyinkinay@msn.com>
2022-11-03 00:35:35 +0300

    git commit -m "Proje Bitimi"

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int int_random = rand.nextInt(11);
+        Scanner scan = new Scanner (System.in);
+        System.out.println("1-10 arası bir sayıyı tahmin ediniz");
+        while(true){
+try{
+    int sayi =scan.nextInt();
+    if (int_random == sayi) {
+        System.out.println("Tebrikler bildiniz " + int_random);
+        break;
+    } else if (int_random > sayi) {
+        System.out.println("Sayınız küçük,yeniden deneyin");
+    } else {
+        System.out.println("Sayınız büyük, yeniden deneyin");
+    }
+}
+catch (Exception e){
+    System.out.println("Sayı girmelisiniz, yeniden başlatın");
+    break;
+}
+        }
+
+        }
+
+}
